d97a577f4201949ee1473f8fa8d96152c2cc6fb5
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-03-14 18:48:01 -0300

    Corregir types y modificar App

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,24 @@ import React, { Suspense } from 'react'
 import { Route, Switch } from 'wouter'
 import PageLoader from './components/PageLoader'
 import './App.css'
+import Home from './pages/Home'
 
-const Home = React.lazy(() => import('./pages/Home'))
-const Game = React.lazy(() => import('./pages/Game'))
 const Error404 = React.lazy(() => import('./pages/Error404'))
+const Game = React.lazy(() => import('./pages/Game'))
 
-function App () {
-  return (
-		<Suspense fallback={<PageLoader />}>
-			<Switch>
+function App() {
+	return (
+		<Switch>
+			<Route path="/" component={Home} />
+			<Suspense fallback={<PageLoader />}>
 				<Route path="/game/time_trial">
 					<Game trial />
 				</Route>
 				<Route path="/game" component={Game} />
-				<Route path="/" component={Home} />
-				<Route component={Error404} />
-			</Switch>
-		</Suspense>
-  )
+				<Route path="/:rest*" component={Error404} />
+			</Suspense>
+		</Switch>
+	)
 }
 
 export default App
