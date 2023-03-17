e2ef66cf44266fb8d706abfb679a48c58751a051
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-03-17 13:29:58 -0300

    Instalar dependencias y vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 /// <reference types="vitest"  />
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    coverage: { provider: 'c8' }
-  }
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		coverage: { provider: 'c8' }
+	}
 })
